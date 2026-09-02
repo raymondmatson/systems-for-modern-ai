69ff22431cf7899986807b2fe3fd2680f737eb06
raymondmatson <69773556+raymondmatson@users.noreply.github.com>
2026-09-01 20:11:45 -0700

    Configured Vite for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,2 @@
 import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';
-export default defineConfig({base:'./',plugins:[react()],server:{port:5173},preview:{port:4173}});
+export default defineConfig({base:"/systems-for-modern-ai/",plugins:[react()],server:{port:5173},preview:{port:4173}});
